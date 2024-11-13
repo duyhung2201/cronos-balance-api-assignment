74e2866d0623227e9461c2ad087f451c355d9f73
DuyHungLe <duyhung2201@gmail.com>
2024-11-13 18:21:13 -0500

    feat: add token balance api

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { balanceController } from './controllers';
+import { balanceController, tokenBalanceController } from './controllers';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/balance/:address', balanceController);
+app.get('/token-balance/:address/:tokenAddress', tokenBalanceController);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- src/controllers.ts
+++ src/controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { getBalance } from './blockchain-services';
+import { getBalance, getTokenBalance } from './blockchain-services';
 
 export const balanceController = async (req: Request, res: Response) => {
   try {
@@ -10,3 +10,16 @@ export const balanceController = async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Error fetching balance' });
   }
 };
+
+export const tokenBalanceController = async (req: Request, res: Response) => {
+  try {
+    const { address, tokenAddress } = req.params;
+    const balance = await getTokenBalance(address, tokenAddress);
+    res.json({ address, balance });
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching token balance' });
+  }
+}
+
+// /Error fetching token balance: TypeError: Cannot convert undefined to a BigInt
+// curl http://localhost:3000/token-balance/0x0862b415323A432f57F2E7cA5171DD0f38130D79/0xcCcCcCcCdbEC186DC426F8B5628AF94737dF0E6
