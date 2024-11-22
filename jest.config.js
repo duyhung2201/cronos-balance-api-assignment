d599076a391e66014eec01788008d6eb5900b841
DuyHungLe <duyhung2201@gmail.com>
2024-11-21 20:09:55 -0500

    feat: enable test coverage reporting in Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	testMatch: ['**/tests/**/*.test.ts'],
+	collectCoverage: true,
+	coverageThreshold: {
+		global: {
+			branches: 90,
+			functions: 90,
+			lines: 90,
+			statements: 90,
+		},
+	},
 };
